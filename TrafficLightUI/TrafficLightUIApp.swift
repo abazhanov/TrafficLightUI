e38a48be2a18ac06057ba9e93d48fe682af02c33
Artem Bazhanov <abazhanov@yahoo.com>
2021-06-01 13:05:01 +0300

    Initial Commit

---
+++ TrafficLightUI/TrafficLightUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLightUIApp.swift
+//  TrafficLightUI
+//
+//  Created by Artem Bazhanov on 01.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLightUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
