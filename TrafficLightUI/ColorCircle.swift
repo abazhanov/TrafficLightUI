0dd673bc554f217845f7ac66726d4cf9e2cb555b
Artem Bazhanov <abazhanov@yahoo.com>
2021-06-01 16:49:35 +0300

    Completely done

---
+++ TrafficLightUI/ColorCircle.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  TrafficLightUI
+//
+//  Created by Artem Bazhanov on 01.06.2021.
+//
+
+import SwiftUI
+
+struct ColorCircle: View {
+    let color: Color
+    
+    var body: some View {
+        Circle()
+            .foregroundColor(color)
+            .frame(width: 100, height: 100)
+            .overlay(Circle().stroke(Color.white, lineWidth: 4))
+            .shadow(radius: 10)
+    }
+}
+
+struct ColorCircle_Previews: PreviewProvider {
+    static var previews: some View {
+        ColorCircle(color: .blue)
+    }
+}
--- TrafficLightUI/ContentView.swift
+++ TrafficLightUI/ContentView.swift
@@ -7,10 +7,58 @@
 
 import SwiftUI
 
+enum CurrentLight {
+    case red, yellow, green
+}
+
 struct ContentView: View {
+    @State private var redColor: Color = .red.opacity(0.3)
+    @State private var greenColor: Color = .green.opacity(0.3)
+    @State private var yellowColor: Color = .yellow.opacity(0.3)
+    
+    @State private var currentLight = CurrentLight.red
+    
+    @State private var nameButton = "START"
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Color(.black)
+                .ignoresSafeArea()
+            VStack {
+                ColorCircle(color: redColor)
+                    .padding(.bottom, 10)
+                ColorCircle(color: yellowColor)
+                    .padding(.bottom, 10)
+                ColorCircle(color: greenColor)
+                Spacer()
+                Button(action: {
+                    if nameButton == "START" {
+                        nameButton = "NEXT"
+                    }
+                    switch currentLight {
+                    case .red:
+                        greenColor = .green.opacity(0.3)
+                        redColor = .red.opacity(1)
+                        currentLight = .yellow
+                    case .yellow:
+                        redColor = .red.opacity(0.3)
+                        yellowColor = .yellow.opacity(1)
+                        currentLight = .green
+                    case .green:
+                        yellowColor = .yellow.opacity(0.3)
+                        greenColor = .green.opacity(1)
+                        currentLight = .red
+                    }
+                }) {
+                    Text(nameButton)
+                        .font(.title)
+                        .frame(width: 130, height: 50)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 20)
+                                .stroke(Color.white, lineWidth: 4))
+                }
+            }
+        }
     }
 }
 
